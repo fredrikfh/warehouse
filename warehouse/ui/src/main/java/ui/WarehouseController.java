e76931ad9328bbe5173360ba7aa462cd60e9c181
Fredrik <fredrfha>
2021-11-03 10:10:01 +0100

    fix checkstyle

--- warehouse/ui/src/main/java/ui/WarehouseController.java
+++ warehouse/ui/src/main/java/ui/WarehouseController.java
@@ -125,7 +125,7 @@ public class WarehouseController implements WarehouseListener {
       notHover(itemElement, index);
     }
 
-    if (warehouse.getCurrentUser() != null){
+    if (warehouse.getCurrentUser() != null) {
       addItemButton.setDisable(false);
     } else {
       addItemButton.setDisable(true);
